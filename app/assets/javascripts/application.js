ad348a6341199fba8dd2a8705e83a0aec55514e7
Mike Land <mikepland@gmail.com>
2014-05-12 01:50:54 -0500

    fixes conditional logic

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,44 +20,50 @@ function EvaluatePassNoPass($scope, total_of_dice)
     console.log($scope.game_status)
     console.log($scope.point_is)
     console.log(total_of_dice)
-	if ($scope.game_status == "Come Out Roll" && total_of_dice == ( 2 || 3 || 12 )) {
+	if ($scope.game_status == "Come Out Roll") {
+    if (total_of_dice ==  2 || total_of_dice == 3 || total_of_dice == 12 ) {
 			$scope.dealer_call = "Craps Line Away "
       $scope.the_call_is = total_of_dice
-	}
-  else if ($scope.game_status == "Come Out Roll" && total_of_dice == ( 7 || 11 )) {
+    } 
+    else if (total_of_dice == 7 || total_of_dice == 11) {
 			$scope.dealer_call = "Front Line Winner"
       $scope.the_call_is = total_of_dice
-	}
-	else if ($scope.game_status == "Come Out Roll" && total_of_dice != (2 || 3 || 7 || 11 || 12)) {
+    }
+    else {
       $scope.dealer_call = "We have a point "
       $scope.game_status = "Point is "
       $scope.point_is = total_of_dice
       $scope.the_call_is = total_of_dice
-	}
-	else if ($scope.game_status == "Point is " && total_of_dice == $scope.point_is) {
+    }
+  }
+	else if ($scope.game_status == "Point is ") {
+    if ($scope.point_is == total_of_dice) {
 			$scope.dealer_call = "Frontline Winner"
 			$scope.game_status = "Come Out Roll"
       $scope.point_is = ""
       $scope.the_call_is = total_of_dice
-	}
-	else if ($scope.game_status == "Point is " && total_of_dice == 7) {
+    }
+    else if (total_of_dice == 7) {
 			$scope.dealer_call = "Seven Out"
 			$scope.game_status = "Come Out Roll"
       $scope.point_is = ""
       $scope.the_call_is = total_of_dice
-	}
-	else {
+    }
+    else {
       $scope.the_call_is = total_of_dice
+    }
 	}
 }
 
 function RollDice($scope) {
   $scope.game_status = "Come Out Roll"
-  $scope.roll = function(){
+  $scope.roll = function() {
     var current_roll_dice_2= new Array(1,2,3,4,5,6);
     var random_2 = current_roll_dice_2[Math.floor(Math.random() * current_roll_dice_2.length)];
+    $scope.die_one = random_2;
     var current_roll_dice_1= new Array(1,2,3,4,5,6);
     var random_1 = current_roll_dice_1[Math.floor(Math.random() * current_roll_dice_1.length)];
+    $scope.die_two = random_1;
     var total_of_dice = random_1 + random_2;
     EvaluatePassNoPass($scope, total_of_dice);
   };
