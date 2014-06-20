83db99b24e279c49ca141a6fcddc7bb310c14db7
Mike Land <mikepland@gmail.com>
2014-06-19 20:43:14 -0500

    sets up winners and losers for notices on props

--- app/assets/javascripts/odds.js
+++ app/assets/javascripts/odds.js
@@ -46,18 +46,60 @@ function PropBets($scope, random_1, random_2) {
         var prop_var_2 = pair_of_props_vars[2]
       
         if ($scope[bet_actual] > 0) {
-             if ((prop_var_1 == random_1 && prop_var_2 == random_2) || (prop_var_1 == random_2 && prop_var_2 == random_1)) {
-                  var stay_up = $scope[bet_actual]
-                  var highside_payout = 30
-                  var lowside_payout = 15
-                  var stay_up_payout = $scope[bet_actual] * (random_1 == random_2 ? highside_payout : lowside_payout)
-                  $scope.bank_roll_actual += stay_up_payout
-                  //PlayerGameCalls($scope, $scope[bet_actual], "WON", game_call_actual, stay_up, stay_up_payout)
-             } else {
-                  //PlayerGameCalls($scope, $scope[bet_actual], "LOST", game_call_actual)
-                  $scope.bank_roll_actual -= $scope[bet_actual]
-                  $scope[bet_actual] = 0
-             }
+            var game_helper_modal_headline = "Prop Bet on "+bet_actual.slice(-3).slice(0,1)+bet_actual.slice(-1)
+            var game_helper_modal_win_lose = "The Field is a One Roll Bet. It wins on any of the following numbers 2,3,4,9,10,11,12. Field Pays Even Money (2 pays double and 12 pays triple)"
+            if ((prop_var_1 == random_1 && prop_var_2 == random_2) || (prop_var_1 == random_2 && prop_var_2 == random_1)) {
+                if (prop_var_1 == 1 && prop_var_2 == 1) {
+                    var game_helper_modal_headline = "Prop Bet on Aces"
+                    var game_helper_modal_win_lose = "Aces is a One Roll Bet. The dice must come 1-1 in order to win. Pays 31 for 1"
+                } else if ((prop_var_1 == 5 && prop_var_2 == 6) || (prop_var_1 == 6 && prop_var_2 == 5)) {
+                    var game_helper_modal_headline = "Prop Bet on Yo"
+                    var game_helper_modal_win_lose = "Ace Deuce is a One Roll Bet. The dice must come 6-5 or 5-6 in order to win. Pays 16 for 1"
+                } else if ((prop_var_1 == 1 && prop_var_2 == 2) || (prop_var_1 == 2 && prop_var_2 == 1)) {
+                    var game_helper_modal_headline = "Prop Bet on Ace Deuce"
+                    var game_helper_modal_win_lose = "Ace Deuce is a One Roll Bet. The dice must come 2-1 or 1-2 in order to win. Pays 16 for 1"
+                } else if (prop_var_1 == 6 && prop_var_2 == 6) {
+                    var game_helper_modal_headline = "Prop Bet on Twelve"
+                    var game_helper_modal_win_lose = "Twelve is a One Roll Bet. The dice must come 6-6 in order to win. Pays 31 for 1"
+                } else {
+                    var highside_lowside = (prop_var_1 == prop_var_2 ? " 31 for 1" : " 16 for 1")
+                    var game_helper_modal_headline = "Prop Bet on "+prop_var_1+"-"+prop_var_2+" Hopping"
+                    var highside_lowside_wording = (prop_var_1 == prop_var_2 ? (prop_var_1+"-"+prop_var_2) : (prop_var_1+"-"+prop_var_2+" or "+prop_var_2+"-"+prop_var_1))
+                    var game_helper_modal_win_lose = prop_var_1+"-"+prop_var_2+" Hopping is a One Roll Bet. The dice must come "+highside_lowside_wording+" in order to win. Pays "+highside_lowside
+                }
+                var game_helper_modal_id = "#"+bet_actual+"_modal"
+                var game_helper_modal_message = "You Placed a " + game_helper_modal_headline+ " for " + $scope[bet_actual]
+                var stay_up = $scope[bet_actual]
+                var highside_payout = 30
+                var lowside_payout = 15
+                var stay_up_payout = $scope[bet_actual] * (random_1 == random_2 ? highside_payout : lowside_payout)
+                $scope.bank_roll_actual += stay_up_payout
+                PlayerGameCalls($scope, "WON", game_helper_modal_id, game_helper_modal_message, game_helper_modal_headline, game_helper_modal_win_lose, stay_up, stay_up_payout)
+           } else {
+                $scope.bank_roll_actual -= $scope[bet_actual]
+                $scope[bet_actual] = 0
+                if (prop_var_1 == 1 && prop_var_2 == 1) {
+                    var game_helper_modal_headline = "Prop Bet on Aces"
+                    var game_helper_modal_win_lose = "Aces is a One Roll Bet. The dice must come 1-1 in order to win. Pays 31 for 1"
+                } else if ((prop_var_1 == 5 && prop_var_2 == 6) || (prop_var_1 == 6 && prop_var_2 == 5)) {
+                    var game_helper_modal_headline = "Prop Bet on Yo"
+                    var game_helper_modal_win_lose = "Ace Deuce is a One Roll Bet. The dice must come 6-5 or 5-6 in order to win. Pays 16 for 1"
+                } else if ((prop_var_1 == 1 && prop_var_2 == 2) || (prop_var_1 == 2 && prop_var_2 == 1)) {
+                    var game_helper_modal_headline = "Prop Bet on Ace Deuce"
+                    var game_helper_modal_win_lose = "Ace Deuce is a One Roll Bet. The dice must come 2-1 or 1-2 in order to win. Pays 16 for 1"
+                } else if (prop_var_1 == 6 && prop_var_2 == 6) {
+                    var game_helper_modal_headline = "Prop Bet on Twelve"
+                    var game_helper_modal_win_lose = "Twelve is a One Roll Bet. The dice must come 6-6 in order to win. Pays 31 for 1"
+                } else {
+                    var highside_lowside = (prop_var_1 == prop_var_2 ? " 31 for 1" : " 16 for 1")
+                    var game_helper_modal_headline = "Prop Bet on "+prop_var_1+"-"+prop_var_2+" Hopping"
+                    var game_helper_modal_id = "#"+bet_actual+"_modal"
+                    var game_helper_modal_message = "You Placed a " + game_helper_modal_headline
+                    var highside_lowside_wording = (prop_var_1 == prop_var_2 ? (prop_var_1+"-"+prop_var_2) : (prop_var_1+"-"+prop_var_2+" or "+prop_var_2+"-"+prop_var_1))
+                    var game_helper_modal_win_lose = prop_var_1+"-"+prop_var_2+" Hopping is a One Roll Bet. The dice must come "+highside_lowside_wording+" in order to win. Pays "+highside_lowside
+                }
+                PlayerGameCalls($scope, "LOST", game_helper_modal_id, game_helper_modal_message, game_helper_modal_headline, game_helper_modal_win_lose)
+           }
         }
     })
 
