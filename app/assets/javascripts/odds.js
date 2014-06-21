8cc2d1a3985db2875bfc3b0a38299c9e72aae6d0
Mike Land <mikepland@gmail.com>
2014-06-21 15:01:16 -0500

    removes totals

--- app/assets/javascripts/odds.js
+++ app/assets/javascripts/odds.js
@@ -1,6 +1,5 @@
 function EvaluateTheField($scope, total_of_dice) {
     if ($scope.field_bet > 0) {
-        var game_notice_actual_before = $scope.bank_roll_actual
         var game_helper_modal_headline = "Field Bet"
         var game_helper_modal_win_lose = "The Field is a One Roll Bet. It wins on any of the following numbers 2,3,4,9,10,11,12. Field Pays Even Money (2 pays double and 12 pays triple)"
         if (total_of_dice == 2) {
@@ -30,7 +29,6 @@ function EvaluateTheField($scope, total_of_dice) {
             var game_helper_modal_message = "The Dice Rolled " + total_of_dice + ". This loses in the field." 
             PlayerGameCalls($scope, "LOST", "#field_bet_modal", game_helper_modal_message, game_helper_modal_headline, game_helper_modal_win_lose)
         }
-    $scope.archive_game_calls.push({game_notice_actual: "Field Bet", net_change: (($scope.bank_roll_actual - game_notice_actual_before)+" Coins "), winning_bet: ($scope.bank_roll_actual - game_notice_actual_before > 0 ? true : false), losing_bet: ($scope.bank_roll_actual - game_notice_actual_before < 0 ? true : false)})
     }
 }
 
@@ -103,7 +101,7 @@ function PropBets($scope, random_1, random_2) {
                 }
                 PlayerGameCalls($scope, "LOST", game_helper_modal_id, game_helper_modal_message, game_helper_modal_headline, game_helper_modal_win_lose)
            }
-           $scope.archive_game_calls.push({game_notice_actual: game_helper_modal_headline, net_change: (($scope.bank_roll_actual - game_notice_actual_before)+" Coins "), winning_bet: ($scope.bank_roll_actual - game_notice_actual_before > 0 ? true : false), losing_bet: ($scope.bank_roll_actual - game_notice_actual_before < 0 ? true : false)})
+           $scope.last_roll_totals.push({game_notice_actual: game_helper_modal_headline, net_change: (($scope.bank_roll_actual - game_notice_actual_before)+" Coins "), winning_bet: ($scope.bank_roll_actual - game_notice_actual_before > 0 ? true : false), losing_bet: ($scope.bank_roll_actual - game_notice_actual_before < 0 ? true : false)})
         }
     })
 
