e669ab627a0489ed9a14611a2faa93c45aed15bb
Mike Land <mikepland@gmail.com>
2014-06-22 13:31:53 -0500

    Adjusts code to allow for winning come bets with no new come place

--- app/assets/javascripts/game_status.js
+++ app/assets/javascripts/game_status.js
@@ -228,7 +228,7 @@ function ComesGoToThe($scope, total_of_dice) {
             var game_helper_modal_headline = " the Odds for your Come Bet"
             var game_helper_modal_win_lose = come_bet_message
             var game_helper_modal_id = "#come_bet_off_and_on_on_"+scopeNum[total_of_dice]+"_modal"
-            var game_helper_modal_message = "Your Come Bet on "+writtenWord[total_of_dice]+" with " + stay_up + " Odds, The Odds paid "+stay_up_payout+". The original Come Bet (or Flat) always pays even money. Total Payout was "+(stay_up_payout+stay_up+$scope['come_bet_flat_on_'+scopeNum[total_of_dice]])
+            var game_helper_modal_message = "Your Come Bet on "+writtenWord[total_of_dice]+" with " + stay_up + " Odds, The Odds paid "+stay_up_payout+". The original Come Bet (or Flat) always pays even money. Total Payout was "+(stay_up_payout+$scope['come_bet_flat_on_'+scopeNum[total_of_dice]])+", Your bet is still up to Win."
             PlayerGameCalls($scope, "WON", game_helper_modal_id, game_helper_modal_message, game_helper_modal_headline, game_helper_modal_win_lose, stay_up, stay_up_payout)
         } else {
             var stay_up = $scope['come_bet_flat_on_'+scopeNum[total_of_dice]]
@@ -239,9 +239,7 @@ function ComesGoToThe($scope, total_of_dice) {
             var game_helper_modal_message = "Your Come Bet on "+writtenWord[total_of_dice]+" Pays Even Money. The Odds pay a higer rate than Even Money. Once a Come Bet has traveled, click on it to place Odds."
             PlayerGameCalls($scope, "WON", game_helper_modal_id, game_helper_modal_message, game_helper_modal_headline, game_helper_modal_win_lose, stay_up, stay_up_payout)
         }
-    }
-
-    if (($scope['come_bet_flat_on_'+scopeNum[total_of_dice]] != $scope.place_come_bet) && $scope.place_come_bet != 0) {
+    } else if ($scope['come_bet_flat_on_'+scopeNum[total_of_dice]] != $scope.place_come_bet) {
         if ($scope['come_bet_flat_on_'+scopeNum[total_of_dice]] != 0) {
             var stay_up = ($scope['come_bet_odds_on_'+scopeNum[total_of_dice]])
             var stay_up_payout= ($scope['come_bet_odds_on_'+scopeNum[total_of_dice]] * trueOdds[total_of_dice])
