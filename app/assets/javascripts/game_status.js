f49a044ed1fa4c56631070dba09fc62351dae016
Mike Land <mikepland@gmail.com>
2014-06-22 23:50:00 -0500

    empty commit

--- app/assets/javascripts/game_status.js
+++ app/assets/javascripts/game_status.js
@@ -157,9 +157,9 @@ function SetsThePoint($scope, total_of_dice) {
         $scope['come_bet_flat_on_'+scopeNum[total_of_dice]] = 0
     }
     $scope[writtenWord[total_of_dice]] = "ON"
-    var game_helper_modal_message = "We Now Have A Point Of "+writtenWordCap[total_of_dice] 
+    var game_helper_modal_message = ($scope.line_bet > $scope.dont_pass_line_bet ? (writtenWordCap[total_of_dice] + " Rolls You Win, Seven Rolls you Lose") : (" Seven Rolls You Win, " + writtenWordCap[total_of_dice] + " Rolls you Lose"))
     var game_helper_modal_win_lose = "It was just the \"Come Out Roll\" Now we have a point of "+writtenWordCap[total_of_dice]+"."
-    var game_helper_modal_headline = "Point is "+writtenWordCap[total_of_dice]
+    var game_helper_modal_headline = "Point is "+writtenWordCap[total_of_dice]+"."
     var game_helper_modal_id = "#point_is_set_no_come_bet_modal"
     PlayerGameCalls($scope, "INFO", game_helper_modal_id, game_helper_modal_message, game_helper_modal_headline, game_helper_modal_win_lose)
 }
